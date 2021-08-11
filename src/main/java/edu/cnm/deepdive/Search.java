1bfd2763585115143bf722c862dadd679ced8a25
Kelley <kbonneville@cnm.edu>
2021-08-11 12:25:18 -0600

    begin implementation.

--- src/main/java/edu/cnm/deepdive/Search.java
+++ src/main/java/edu/cnm/deepdive/Search.java
@@ -1,6 +1,6 @@
 package edu.cnm.deepdive;
 
-public class Search {
+public class Search<position> {
 
   public static int linearSearch(int[] haystack, int needle) {
    int position = -1;
@@ -11,9 +11,29 @@ public class Search {
        break;
      }
    }
-
    return position;
-
   }
+    public static int binarySearch(int[] haystack, int needle) {
+    return binarySearch(haystack, needle, 0, haystack.length);
+  }
+  private static int binarySearch(int[] haystack, int needle, int start, int end) {
+    int position = ~start;
+    if (end > start)
+      while = (start + end) / 2;
 
+    int mid = 0;
+    int value = haystack[mid];
+
+      if (value == needle) {
+        position = mid;
+      } else if (value < needle) {
+        position = binarySearch(haystack, needle, start, mid + 1, end);
+      } else {
+        position = binarySearch(haystack, needle, start, mid);
+      }
+    }
+  private static void binarySearch(int[] haystack, int needle, int start, int i, int end) {
+  }
+  return position;
 }
+
